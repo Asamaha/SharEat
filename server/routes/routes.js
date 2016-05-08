0e45c3944861350de3efe37e3fefdfeae4090a49
Adam Samaha <adamsamahaa@gmail.com>
2016-05-08 10:28:48 -0700

    Setup dependences

---
+++ server/routes/routes.js
@@ -0,0 +1,17 @@
+// Setup dependences
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var twitterApiController = require('../controllers/twitterApiController.js');
+var UserController = require('../controllers/userController.js');
+var passport = require('passport');
+
+var isLoggedIn = function(req, res, next) {
+
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/signup');
+};
