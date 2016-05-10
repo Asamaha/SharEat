2f057f2c062bc8b37e7f323a10f9db3d2c71d904
Adam Samaha <adamsamahaa@gmail.com>
2016-05-10 12:52:00 -0700

    create array that will contain data for ALL incoming tweets

--- client/public/controllers/mapsPageController .js
+++ client/public/controllers/mapsPageController .js
@@ -1,1 +1,25 @@
+app.controller('mapsPageController', ['$scope', '$http', 'httpService', '$sce', "$timeout", function ($scope, $http, httpService, $sce, $timeout) {
+
+  //////////////////////////////////////////MAPS PAGE CONTROLLER DRIVER//////////////////////////////////////////////
+
+  //create array that will contain data for ALL incoming tweets
+  $scope.allTweets = {
+    data: []
+  };
+
+  //create array that will contain data for ONLY quality/relevant tweets
+  $scope.relevantTweets = [];
+
+  //function that will ultimately 
+  $scope.favoriteSubmit = function () {
+    httpService.sendFavorite($scope.favoriteField);
+  };
+
+  //used for relevantTweets
+  $scope.addRelevantTweet = function(tweet){
+    $scope.relevantTweets.unshift(tweet);
+  }
+
+  
+  }]);
 
