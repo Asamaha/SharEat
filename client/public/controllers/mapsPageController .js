e06f2db625308a280448479792f70ae510596f5b
Adam Samaha <adamsamahaa@gmail.com>
2016-05-27 11:46:16 -0700

    determine content added to info window on each marker

--- client/public/controllers/mapsPageController .js
+++ client/public/controllers/mapsPageController .js
@@ -19,7 +19,8 @@ app.controller('mapsPageController', ['$scope', '$http', 'httpService', '$sce', 
   $scope.addRelevantTweet = function(tweet){
     $scope.relevantTweets.unshift(tweet);
   }
-////////////////////////////////////////////CREATE AND OPEN SOCKET/////////////////////////////////////////////////////////
+
+  ////////////////////////////////////////////CREATE AND OPEN SOCKET/////////////////////////////////////////////////////////
   
   var onInit = function() {
     
@@ -44,6 +45,110 @@ app.controller('mapsPageController', ['$scope', '$http', 'httpService', '$sce', 
       clearMarkers();
       $scope.allTweets.data = [];
     };
-  
-  }]);
 
+    //establish tweet relevancy criteria; 
+    var numOfFollowersToBeRelevant = 10000;
+    var numOfRetweetsToBeRelevant = 50; 
+    var maxNumOfRelevantTweetsAllowed = 15;
+
+
+    //////////////////////////////////////////SET UP HEAT MAP///////////////////////////////////////////////////
+    $timeout(function(){
+      heatmap.setMap(window.map);
+    }, 10);
+ 
+    //////////////////////////////////////////CONNECT TO SOCKET///////////////////////////////////////////////
+    if(io !== undefined) {
+      //connects to socket
+      var socket = io.connect();
+      //uses socket to listen for incoming tweet stream 
+      
+      //code is a little buggy, but should offer a good start for doing the following when a search request is submitted:
+      // a) clearing the map, b) emitting a filter request to the stream and c) re-starting the heatmap
+      // $scope.submitSearch = function () {
+      //   deleteMarkers();
+      //   heatmap.setMap(null);
+      //   socket.emit("filter", $scope.searchField);
+      //   console.log($scope.searchField);
+      //   heatmap = new google.maps.visualization.HeatmapLayer({
+      //     radius: 15
+      //   });
+      //   heatmap.setMap(window.map);
+      // };
+
+      socket.on('tweet-stream', function (data) {
+
+        if($scope.allTweets.data.length > maxNumOfTweetsAllowedOnMap){
+          var pinToRemove = $scope.allTweets.data.shift();
+          pinToRemove.setMap(null);
+        }
+        ///////////////////////////////////DETERMINE RELEVANCE/QUALITY OF TWEET//////////////////////////////////
+        //set relevant parameters
+        var numOfFollowers = data.followers_count;
+        var numOfRetweets = data.retweet_count;
+        var verifiedAccount = data.verified;
+        var tweetObject = {};
+        //if incoming tweet meets relevancy criteria...
+        if((numOfFollowers >= numOfFollowersToBeRelevant) || (numOfRetweets >= numOfRetweetsToBeRelevant)){
+          //then check to see how many relevant tweets are already being displayed on page; if max limit 
+          //has already been reached then pop last item out of relevantsTweets array
+          if($scope.relevantTweets.length === maxNumOfRelevantTweetsAllowed){
+            $scope.relevantTweets.pop();
+          }
+          //for all incoming tweets that match criteria, create a tweetObject that contains most relevant info for tweet
+          //(e.g. handle, content, and time);         
+          tweetObject = {
+            handle: data.handle,
+            text: data.tweetText,
+            time: data.tweetTime
+          };
+          //add latest tweetObject to the beginning of the relevantTweets array;
+          $scope.$apply(function() {
+            $scope.addRelevantTweet(tweetObject);
+          })
+        }
+
+        ///////////////////////////////////PLACE ALL INCOMING TWEETS ON MAP///////////////////////////////////////
+
+        var tweetLocation = new google.maps.LatLng(data["coordinates"]["coordinates"][1], data["coordinates"]["coordinates"][0]);
+        
+        heatmap.data.push(tweetLocation);
+        
+        var tweetMarker = new google.maps.Marker({
+           icon: "client/assets/small-dot-icon.png",
+           position: tweetLocation,
+           map: window.map
+         });
+
+        //determine content added to info window on each marker  
+        var tweetContent = '<div>' + data['name'] + ": " + data['tweetText'] + '</div>';
+        var markerInfoWindow = new google.maps.InfoWindow({
+           content: tweetContent
+         });
+
+        //set up listeners for each tweetMarker...
+        tweetMarker.addListener('mouseover', function () {
+          markerInfoWindow.open(map, tweetMarker);
+         });
+        tweetMarker.addListener('mouseout', function () {
+        markerInfoWindow.close();
+         });
+        // //set tweet on map...
+        tweetMarker.setMap(window.map, tweetLocation, tweetContent);
+
+        $scope.allTweets.data.push(tweetMarker);
+
+      });
+      
+
+      socket.on('connected', function (r) {
+        console.log('connected client');
+        socket.emit('tweet flow');
+      });
+    };
+
+  };
+
+  onInit();
+
+}]);
