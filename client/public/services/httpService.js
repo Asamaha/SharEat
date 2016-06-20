7494d4913d78433f3985b51095ae18ac849624ff
Adam Samaha <adamsamahaa@gmail.com>
2016-06-20 13:30:55 -0700

    adding favorite to users list of favorites

---
+++ client/public/services/httpService.js
@@ -0,0 +1,60 @@
+app.service('httpService', ['$http', function ($http) {
+
+  this.signUp = function (username, password, email) {
+
+    var signupUser = {
+      username : username,
+      password : password,
+      email : email
+    };
+
+    //signing up a new user
+    return $http.post("/api/users", signupUser)
+      .then(function (success) {
+        return success.body;
+      }, function (error) {
+        return error;
+        console.log(error);
+      });
+  };
+
+  //adding favorite to users list of favorites
+  this.sendFavorite = function (favorite) {
+
+    var addToFavorites = {
+      favorites: favorite
+    };
+
+    return $http.put("/api/users", addToFavorites)
+      .then(function (success) {
+        return success;
+      }, function (error) {
+        return error;
+      });
+  };
+
+  //login functionality
+  this.login = function () {
+    $http.get('/login')
+      .then(function (success) {
+        return success;
+      }, function (error) {
+        return error;
+      });
+  };
+
+  //getting tweets
+  this.getTweets = function (searchTerm) {
+
+    var getRequestGoesTo = '/api/tweets/' + searchTerm;
+
+    return $http.get(getRequestGoesTo)
+      .then(function (success) {
+        return success.data;
+      }, function (error) {
+        return error;
+      });
+  }
+
+
+}]);
